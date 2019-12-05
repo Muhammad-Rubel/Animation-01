3d34dc7c015fbab35ae342999f09983015912240
Mohammad Rubel <rubelsdl88@gmail.com>
2019-12-05 21:26:49 +0600

    Add main file pages

---
+++ script.js
@@ -0,0 +1,47 @@
+// ==================================================================
+//                 Section 2. Variables & Strings Start
+// ==================================================================
+
+// alert("hellow world");
+// document.write("<h1>Hi There</h1>");
+
+// var visitorName = prompt("What is your name?");
+// // document.write("Hi " + visitorName + ", Whelcome to my page");
+// visitorName += ", whelcome to my page";
+// visitorName += ". We are very glad to see you here.";
+// document.write(visitorName);
+// console.log(visitorName.length);
+
+// var visitorName = prompt("What is your name?");
+// visitorName += ", Welcome to my page";
+// document.write(visitorName.toUpperCase());
+
+// var visitorName = prompt("what is your name?");
+// var area = prompt("where do you live?");
+// var job = prompt("what do you do?");
+// var like = prompt("What do you like to do after your work");
+// document.write("Hi, I'am " + visitorName + ". I lives in " + area + ". I am a " + job + ". I like to " + like + " books after my work done.");
+
+// ==================================================================
+//                 Section 2. Variables & Strings Ends
+// ==================================================================
+
+
+
+// ==================================================================
+//                 Section 3. Number Starts 
+// ==================================================================
+
+var secondsPerMinute = 60;
+var minutePerHour = 60;
+var hourPerDay = 24;
+var dayPerWeek = 7;
+var weekPerYear = 52;
+var secondsPerDay = secondsPerMinute * minutePerHour * hourPerDay;
+document.write("There are " + secondsPerDay + " seconds in a Day.");
+
+
+
+
+
+
